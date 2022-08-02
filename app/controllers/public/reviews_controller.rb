c24b7b5237bbd1f6cde01638e60997bfec5004c5
KShoya119 <gondora2003@gmail.com>
2022-08-02 12:34:30 +0000

    [2022/8/2]

--- app/controllers/public/reviews_controller.rb
+++ app/controllers/public/reviews_controller.rb
@@ -11,6 +11,11 @@ class Public::ReviewsController < ApplicationController
     @review = Review.new(review_params)
     @book.customer_id = current_customer.id
     review.save
+    
+    @review_genre_intermediate = ReviewGenreIntermediate.new
+    @review_genre_intermediate.review_id = @review.id
+    @review_genre_intermediate.game_genre_id = game_genre.id
+    review_genre_intermediate.save
     redirect_to 'reviews/index'
   end
 
